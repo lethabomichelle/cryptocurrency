99479eb32a30c26f6ff0437c53e30c08f46e16a6
lethabomichelle <lethabomichelle@gmail.com>
2024-09-04 17:23:18 +0200

    did a fetch function and renamed value

--- src/components/CryptoCards.js
+++ src/components/CryptoCards.js
@@ -8,95 +8,41 @@ import ETH from '../assets/dashboard/ETH.png'
 
 export default function CryptoCards() {
 
-    const [coin, setCoin] = useState([])
-    const [currency, setCurrrency] = useState({
-        name: "usd",
-        symbol: "$"
-    })
+    const [coinList, setCoinList] = useState([])
 
-    const fetchAllCoins = async () => {
+    useEffect(() => {
         const options = {
             method: 'GET',
             headers: { accept: 'application/json', 'x-cg-demo-api-key': 'CG-tkgkpH5cDfLNJPiwc4c7RgQS' }
         };
 
         fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd', options)
-            .then(response => response.json())
-            .then(response => {
-                setCoin(response)
-                console.log(coin);
-            })
-            .catch(err => console.error(err));
-    }
-
+            .then(async response => {
+                const res = await response.json();
+                // console.log('resp', res);
+                setCoinList(res.filter(coin => ['btc', 'ada', 'ltc', 'eth'].includes(coin.symbol)))
+            }).catch(err => console.error(err));
+    }, []);
 
     return (
         <div className="cards" >
-            <div className="card-body">
-                <div>
-                    <img src={btc} alt="icon" style={{ width: '52px', height: '52px' }} />
-                    {/* <i>arrow</i> */}
+
+            {coinList.map((coin) =>
+                <div className="card-body">
                     <div>
-                        <svg width="12" height="6" viewBox="0 0 12 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M11.0099 4.48505L8.60243 2.07755L7.13243 0.600054C6.50993 -0.0224463 5.49743 -0.0224463 4.87493 0.600054L0.989931 4.48505C0.479932 4.99505 0.847431 5.86505 1.55993 5.86505H5.76743H10.4399C11.1599 5.86505 11.5199 4.99505 11.0099 4.48505Z" fill="#1ECB4F" />
-                        </svg>
+                        <img src={coin.image} alt="icon" style={{ width: '52px', height: '52px' }} />
+                        <div>
+                            <svg width="12" height="6" viewBox="0 0 12 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M11.0099 4.48505L8.60243 2.07755L7.13243 0.600054C6.50993 -0.0224463 5.49743 -0.0224463 4.87493 0.600054L0.989931 4.48505C0.479932 4.99505 0.847431 5.86505 1.55993 5.86505H5.76743H10.4399C11.1599 5.86505 11.5199 4.99505 11.0099 4.48505Z" fill="#1ECB4F" />
+                            </svg>
 
-                        <span>+0.25%</span>
+                            <span>{coin.name}</span>
+                        </div>
                     </div>
+                    <h3>${coin.current_price.toFixed(3)}</h3>
+                    <span>{coin.id} - {coin.symbol}</span>
                 </div>
-                <h3>$40.291</h3>
-                <span>Bitcoin - BTC</span>
-            </div>
-
-            <div className="card-body">
-                <div>
-                    <img src={ETH} alt="icon" style={{ width: '52px', height: '52px' }} />
-                    {/* <i>arrow</i> */}
-                    <div>
-                        <svg width="12" height="6" viewBox="0 0 12 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M11.0099 4.48505L8.60243 2.07755L7.13243 0.600054C6.50993 -0.0224463 5.49743 -0.0224463 4.87493 0.600054L0.989931 4.48505C0.479932 4.99505 0.847431 5.86505 1.55993 5.86505H5.76743H10.4399C11.1599 5.86505 11.5199 4.99505 11.0099 4.48505Z" fill="#1ECB4F" />
-                        </svg>
-
-                        <span>+0.25%</span>
-                    </div>
-                </div>
-                <h3>$40.291</h3>
-                <span>Ethereum - ETH</span>
-            </div>
-
-            <div className="card-body">
-                <div>
-                    <img src={ITL} alt="icon" style={{ width: '52px', height: '52px' }} />
-                    {/* <i>arrow</i> */}
-                    <div>
-                        <svg width="12" height="6" viewBox="0 0 12 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M11.0099 4.48505L8.60243 2.07755L7.13243 0.600054C6.50993 -0.0224463 5.49743 -0.0224463 4.87493 0.600054L0.989931 4.48505C0.479932 4.99505 0.847431 5.86505 1.55993 5.86505H5.76743H10.4399C11.1599 5.86505 11.5199 4.99505 11.0099 4.48505Z" fill="#1ECB4F" />
-                        </svg>
-
-                        <span>+0.25%</span>
-                    </div>
-                </div>
-                <h3>$40.291</h3>
-                <span>Litecoin - ITL</span>
-            </div>
-
-            <div className="card-body">
-                <div>
-                    <img src={ADA} alt="icon" style={{ width: '52px', height: '52px' }} />
-                    {/* <i>arrow</i> */}
-                    <div>
-                        <svg width="12" height="6" viewBox="0 0 12 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M11.0099 4.48505L8.60243 2.07755L7.13243 0.600054C6.50993 -0.0224463 5.49743 -0.0224463 4.87493 0.600054L0.989931 4.48505C0.479932 4.99505 0.847431 5.86505 1.55993 5.86505H5.76743H10.4399C11.1599 5.86505 11.5199 4.99505 11.0099 4.48505Z" fill="#1ECB4F" />
-                        </svg>
-
-                        <span>+0.25%</span>
-                    </div>
-                </div>
-                <h3>$40.291</h3>
-                <span>Cardano - ADA</span>
-            </div>
-
+            )}
         </div>
-
     )
 }
--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './dashboard.css'
 
-import adawhite from '../assets/dashboard/adawhite.png'
-import adaGraph from '../assets/dashboard/adaGraph.png'
+// import adawhite from '../assets/dashboard/adawhite.png'
+// import adaGraph from '../assets/dashboard/adaGraph.png'
 import ethGraph from '../assets/dashboard/ethGraph.png'
-import whiteEth from '../assets/dashboard/whiteEth.png'
-import btcGraph from '../assets/dashboard/btcGraph.png'
-import whiteBtc from '../assets/dashboard/whiteBtc.png'
-import itcGraph from '../assets/dashboard/itcGraph.png'
-import whiteItc from '../assets/dashboard/whiteItc.png'
+import whitebtc from '../assets/dashboard/whitebtc.png'
+// import btcGraph from '../assets/dashboard/btcGraph.png'
+// import itcGraph from '../assets/dashboard/itcGraph.png'
 
 
 export default function Transactions() {
+    const [coinList, setCoinList] = useState([])
+
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            headers: { accept: 'application/json', 'x-cg-demo-api-key': 'CG-tkgkpH5cDfLNJPiwc4c7RgQS' }
+        };
+
+        fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd', options)
+            .then(async response => {
+                const res = await response.json();
+                console.log('resp', res);
+                setCoinList(res.filter(coin => ['btc', 'ada', 'ltc', 'eth'].includes(coin.symbol)))
+            }).catch(err => console.error(err));
+    }, []);
+
     return (
         <div className="bottom-container">
             <div className="live-Market">
@@ -22,24 +36,26 @@ export default function Transactions() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            {/* <th scope="row" className="cryptoLogo"><img src={whiteEth} alt="" /></th> */}
-                            <td><img className="cryptoLogo" src={whiteEth} alt="" /></td>
-                            <td >
-                                <h3>Ethereum</h3>
-                                <span>ETH / USDT</span>
-                            </td>
-                            <td >
-                                <p>Change</p>
-                                <span>+14.02%</span>
-                            </td>
-                            <td >
-                                <p>Price</p>
-                                <span>39,786 USD</span>
-                            </td>
-                            <td className="graph"><img src={ethGraph} alt="" /></td>
-                        </tr>
-                        <tr>
+                        {coinList.map((coin) =>
+                            <tr>
+                                <td><img className="cryptoLogo" src={`../assets/dashboard/white` + coin.symbol + '.png'} alt="" /></td>
+                                <td >
+                                    <h3>{coin.name}</h3>
+                                    <span>{coin.symbol.toUpperCase()} / USDT</span>
+                                </td>
+                                <td >
+                                    <p>Change</p>
+                                    <span>{coin.price_change_percentage_24h}</span>
+                                </td>
+                                <td >
+                                    <p>Price</p>
+                                    <span>{coin.current_price.toFixed(3)}</span>
+                                </td>
+                                <td className="graph"><img src={ethGraph} alt="" /></td>
+                            </tr>
+                        )}
+
+                        {/* <tr>
                             <td><img className="cryptoLogo" src={whiteBtc} alt="" /></td>
                             <td >
                                 <h3>Bitcoin </h3>
@@ -86,7 +102,7 @@ export default function Transactions() {
                                 <span>4,786 USD</span>
                             </td>
                             <td className="graph"><img src={adaGraph} alt="" /></td>
-                        </tr>
+                        </tr> */}
                     </tbody>
                 </table>
             </div>
@@ -100,7 +116,7 @@ export default function Transactions() {
                     </thead>
                     <tbody>
                         <tr>
-                            <td><img className="cryptoLogo" src={whiteEth} alt="" /></td>
+                            {/* <td><img className="cryptoLogo" src={whiteEth} alt="" /></td> */}
                             <td >
                                 <h3>Ethereum</h3>
                                 <span>ETH / USDT</span>
@@ -111,7 +127,7 @@ export default function Transactions() {
                             </td>
                         </tr>
                         <tr>
-                            <td><img className="cryptoLogo" src={whiteEth} alt="" /></td>
+                            {/* <td><img className="cryptoLogo" src={whiteEth} alt="" /></td> */}
                             <td >
                                 <h3>Ethereum</h3>
                                 <span>ETH / USDT</span>
@@ -122,7 +138,7 @@ export default function Transactions() {
                             </td>
                         </tr>
                         <tr>
-                            <td><img className="cryptoLogo" src={whiteEth} alt="" /></td>
+                            {/* <td><img className="cryptoLogo" src={whiteEth} alt="" /></td> */}
                             <td >
                                 <h3>Ethereum</h3>
                                 <span>ETH / USDT</span>
@@ -133,7 +149,7 @@ export default function Transactions() {
                             </td>
                         </tr>
                         <tr>
-                            <td><img className="cryptoLogo" src={whiteEth} alt="" /></td>
+                            {/* <td><img className="cryptoLogo" src={whiteEth} alt="" /></td> */}
                             <td >
                                 <h3>Ethereum</h3>
                                 <span>ETH / USDT</span>
