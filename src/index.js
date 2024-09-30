1833f53ee074217b0cb7069a9cd9e3298e493a43
Omolemo Modipane <modipane101@gmail.com>
2024-09-30 09:47:48 +0200

    "Updated React DOM rendering from ReactDOM.render to createRoot"

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 // index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'; // Import createRoot from react-dom/client
 import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const clientId = '684255712035-fopai7v838k92cevfgla54o1634v89p5.apps.googleusercontent.com'; 
 
-ReactDOM.render(
+// Select the root element
+const container = document.getElementById('root');
+const root = createRoot(container); // Create a root for rendering
+
+// Render the application
+root.render(
   <GoogleOAuthProvider clientId={clientId}>
     <App />
-  </GoogleOAuthProvider>,
-  document.getElementById('root')
+  </GoogleOAuthProvider>
 );
 
